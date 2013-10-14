34a2d88d549fdd9554addfa49d1df9563ebba621
kuma_d <arthur_d@yeah.net>
2013-10-14 11:33:11 +0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,22 @@
+/**
+ * loads sub modules and wraps them up into the main module
+ * this should be used for top-level module definitions only
+ */
+define([
+    'angular',
+    'uiRouter',
+    './controllers/index',
+    './directives/index',
+    './filters/index',
+    './services/index'
+], function (ng) {
+    'use strict';
+
+    return ng.module('app', [
+        'app.services',
+        'app.controllers',
+        'app.filters',
+        'app.directives',
+        'ui.router'
+    ]);
+});
---
+++ js/main.js
@@ -0,0 +1,31 @@
+/**
+ * configure RequireJS
+ * prefer named modules to long paths, especially for version mgt
+ * or 3rd party libraries
+ */
+require.config({
+
+    paths: {
+        'domReady': '../lib/requirejs-domready/domReady',
+        'angular': '../lib/angular/angular',
+        "uiRouter": "../lib/angular/angular-ui-router"
+    },
+
+    /**
+     * for libs that either do not support AMD out of the box, or
+     * require some fine tuning to dependency mgt'
+     */
+    shim: {
+        'angular': {
+            exports: 'angular'
+        },
+        'uiRouter':{
+            deps: ['angular']
+        }
+    },
+    
+    deps: [
+        // kick start application... see bootstrap.js
+        './bootstrap'
+    ]
+});
